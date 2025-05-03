7649a47d9ce38c6f9fb598ff4744a7e1773ca5e4
ALOK JHA <140833063+Alokjha2004@users.noreply.github.com>
2025-05-04 00:28:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey="your_api";
+const apiKey="b6da5612defcea064ecf793c6c13f993";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
