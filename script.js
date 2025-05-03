77fb3cebc34171fabeedb5f6f7b92e3c469dbd1e
ALOK JHA <140833063+Alokjha2004@users.noreply.github.com>
2025-05-04 00:30:34 +0530

    add click function

--- script.js
+++ script.js
@@ -41,4 +41,9 @@ async function checkWeather(city){
 }
 searchBtn.addEventListener("click", ()=> {
     checkWeather(searchBox.value);
-})
+});
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
